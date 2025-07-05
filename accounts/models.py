ab8f57102667852b621a76ac0fd682d17f46ba83
Adetayo-taiwo87 <tayotaiwotimi@gmail.com>
2025-07-05 01:03:03 +0100

    setup settings.py, custom user model with roles, and initial migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'student'),
+        ('mentor', 'mentor',),
+        ('admin', 'admin'),
+    )
+    role = models.CharField(max_lenght= 10, choices=ROLE_CHOICES, default='student')
+    def __str__(self):
+        return f"{self.username} ({self.role})"
